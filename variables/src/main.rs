a7dad6cb4885cb12c6240f4c83cff2a3b1359132
pavithrarani <pavithra.be08@gmail.com>
2021-06-15 16:24:45 +0530

    1st exercise

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+	//let mut x = 5;
+	let mut a:i64 = 2147483647;
+
+	a = a+1;
+
+    println!("the value of a = {}",a);
+   
+}
---
+++ variables/src/tuple.rs
@@ -0,0 +1,7 @@
+fn main()
+{
+	let tup: (i32, f64, u8) = (500, 3.5, 1);
+	let (x,y,z) = tup;
+
+	println!("x= {:?}",x );
+}
